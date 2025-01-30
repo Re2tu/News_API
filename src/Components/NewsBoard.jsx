3374ff221a81227433a71ddd86f3add650246cdd
Dumpa Reethika <dumpareethika@gmail.com>
2025-01-30 10:32:37 +0530

    NewsBoard change

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -5,6 +5,7 @@ const NewsBoard = ({category}) => {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
     const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+  
     fetch(url)
       .then(response => response.json())
       .then(data => setArticles(data.articles));
